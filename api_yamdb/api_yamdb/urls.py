e3be2f14d3f1277d6a00138396c7b61c0fe32a99
Александр Саратов <Alx.Srtv@yandex.ru>
2022-05-02 23:03:37 +0300

    PEP8 done

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -31,9 +31,9 @@ urlpatterns = [
 
 urlpatterns += [
     url(r'^swagger(?P<format>\.json|\.yaml)$',
-       schema_view.without_ui(cache_timeout=0), name='schema-json'),
+        schema_view.without_ui(cache_timeout=0), name='schema-json'),
     url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0),
-       name='schema-swagger-ui'),
+        name='schema-swagger-ui'),
     url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0),
-       name='schema-redoc'),
+        name='schema-redoc'),
 ]
